79a04fe67db0aab333b26a7a78e992d30fc1c732
lemon5227 <yan2000926@gmail.com>
2021-08-15 17:10:51 +0800

    update

---
+++ Code_Java/实验五/src/DemoPhone.java
@@ -0,0 +1,9 @@
+public class DemoPhnone {
+    public static void main(String[] args){
+        Phone p1 =new Phone();
+        System.out.println(p1.getBand()+"--"+ p1.getPrice()+"--"+ p1.getColor());
+        System.out.println("----------------");
+
+
+    }
+}
